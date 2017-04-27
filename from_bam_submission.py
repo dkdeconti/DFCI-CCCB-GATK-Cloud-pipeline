591998848154dccb0781575021f5994eb8975876
Derrick DeConti <deconti.d@gmail.com>
2017-04-26 22:04:04 -0400

    Simplified templating of input files.

--- from_bam_submission.py
+++ from_bam_submission.py
@@ -1,18 +1,44 @@
 import argparse
+import random
+import string
 from string import Template
+
+
+def create_inputs_json(template_json, bucket, sample_name, bam_file):
+    '''
+    Injects variables into template json for inputs.
+    '''
+    bar_code = ''.join(random.SystemRandom().choice(string.ascii_letters +
+                                                    string.digits)
+                       for _ in range(10))
+    inputs_filename = '.'.join([sample_name, bar_code, "inputs.json"])
+    d = {"BUCKET_INJECTION": bucket,
+         "BAM_INJECTION": bam_file,
+         "INPUT_BASENAME_INJECTION": sample_name}
+    with open(template_json) as filein: s = Template(filein.read())
+    result = s.substitute(d)
+    with open(inputs_filename, 'w') as handle:
+        handle.write(result)
+    return inputs_filename
 
 
 def submit_variant_calling(sample_name, bam_file, bucket, template_json):
     '''
     Submission script for individual bam.
     '''
-    d = {"BUCKET_INJECTION": bucket,
-         "BAM_INJECTION": bam_file,
-         "INPUT_BASENAME_INJECTION": sample_name}
-    filein = open(template_json)
-    s = Template(filein.read())
-    result = s.substitute(d)
-    print result
+    inputs_file = create_inputs_json(template_json, bucket,
+                                     sample_name, bam_file)
+    with open("") as filein:
+        
+    return
+    
+
+def map_bam_tsv(tsv):
+    '''
+    Maps sample names to bam files.
+    '''
+    with open(tsv) as filein:
+        return dict(s.split('\t') for s in filein.read().split('\n'))
 
 
 def main():
@@ -24,10 +50,14 @@ def main():
                         help="sample name and BAM TSV file")
     parser.add_argument("bucketname", metavar="BucketName",
                         help="google bucket name")
-    parser.add_argument("foo", metavar="foo", help="foo")
+    parser.add_argument("", metavar="foo", help="foo")
     parser.set_defaults()
     args = parser.parse_args()
-    submit_variant_calling(args.bamtsv, args.foo, args.bucketname, "input_template.json")
+    samples = map_bam_tsv(args.bamtsv)
+    codes = [submit_variant_calling(sample_name, bam, args.bucketname,
+                                    "input_template.json")
+             for sample_name, bam in samples.items()]
+    print codes
 
 
 if __name__ == "__main__":
