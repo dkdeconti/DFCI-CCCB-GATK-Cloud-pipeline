c97542f228ad72635e7f5309c83edcf030535f53
Derrick DeConti <deconti.d@gmail.com>
2017-05-02 16:49:24 -0400

    More debug and cleanup.

--- GATK_from_bam/HG00096.947j2EFFgr.submission.sh
+++ GATK_from_bam/HG00096.947j2EFFgr.submission.sh
@@ -4,6 +4,6 @@ gcloud alpha genomics pipelines run \
 --logging gs://dfci-cccb-pipeline-testing/logging \
 --inputs WDL=gs://dfci-cccb-pipeline-testing/default_templates/GATK_variant_discovery_from_BAM/GATK_variant_calling.from_bam.wdl \
 --inputs-from-file WORKFLOW_INPUTS=HG00096.947j2EFFgr.inputs.json \
---inputs WORKFLOW_OPTIONS=gs://dfci-cccb-pipeline-testing/default_templates/GATK_variant_discovery_from_BAM/default.options.json \
+--inputs-from-file WORKFLOW_OPTIONS=default.options.json \
 --inputs WORKSPACE=gs://dfci-cccb-pipeline-testing/workspace \
 --inputs OUTPUTS=gs://dfci-cccb-pipeline-testing/outputs
