15de2072bbfa8fe7113373e15e62550a58ea2ba4
Derrick DeConti <deconti.d@gmail.com>
2017-08-07 15:34:15 -0400

    Synced code from web app to local.

--- WDL_Pipelines/GATK_from_bam/tasks.py
+++ WDL_Pipelines/GATK_from_bam/tasks.py
@@ -15,13 +15,15 @@ from client_setup.models import Project
 import pandas as pd
 import yaml
 
-LINK_ROOT = 'https://storage.cloud.google.com/%s/%s' #TODO put this in settings.py?  can a non-app access?
+LINK_ROOT = 'https://storage.cloud.google.com/%s/%s'
+#TODO put this in settings.py?  can a non-app access?
 
 @task(name='check_completion')
-def check_completion(codes):
-    codes
-    while True:
-        for code in codes:
+def check_completion(project, code_map, bucket_name):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(bucket_name)
+    while code_map:
+        for code, samplename in code_map.items():
             script = ' '.join(["gcloud alpha genomics operations describe",
                                code,
                                "--format='yaml(done, error, metadata.events)'"])
@@ -32,8 +34,25 @@ def check_completion(codes):
             out_map = yaml.safe_load(stdout)
             done_status = out_map["done"]
             #errors[code] = out_map["error"]
-            if done_status = True
-        
+            if done_status == True:
+                public_link = LINK_ROOT % (bucket.name, samplename)
+                r = Resource(project=project,
+                             basename=samplename,
+                             public_link=public_link,
+                             resource_type='VCF files')
+                r.save()
+                del code_map[code]
+    message_html = """
+    <html>
+    <body>
+    Your variant calling analysis has finished. Log-in to download your results.
+    </body>
+    </html>
+    """
+    email_utils.send_email(message_html, [project_owner,], \
+                           '[CCCB] Variant calling analysis completed')
+
+
 
 @task(name='deseq_call')
 def check_analysis_complete(deseq_cmd, results_dir, cloud_dge_dir, contrast_name, bucket_name, project_pk):
